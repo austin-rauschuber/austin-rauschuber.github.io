e75b7c860cf384b95d680666e27dd229b702aa58
Austin Rauschuber <arauschu@purdue.edu>
2015-03-05 17:32:24 -0500

    Added mobile swipe based traversal to page

--- main.js
+++ main.js
@@ -35,6 +35,48 @@ var main = function(){
         
     });
     
+    $('.mainContent').on("swipeleft", function(event){
+        var $current = $('.current');
+        var textIndex = $('.textbox').index($current);
+        var index = textIndex+1;
+        if (index>3)
+            index=0;
+        
+        var $newCurrent = $('.textbox').eq(index);
+        
+        $('.currentblock').removeClass('currentblock');
+        $('.block').eq(index).addClass('currentblock');
+        
+        $newCurrent.css({left:"10000px"});
+        $newCurrent.addClass('current');
+
+
+        $newCurrent.animate({left:"0px"},1000,function(){});
+        $current.animate({left:"-10000px"},1000,function(){
+            $current.removeClass('current');
+        });
+    });
+    
+    $('.mainContent').on("swiperight", function(event){
+         var $current = $('.current');
+        var textIndex = $('.textbox').index($current);
+        var index = textIndex-1;
+        if (index < 0)
+            index=3;
+        var $newCurrent = $('.textbox').eq(index);
+        
+        $('.currentblock').removeClass('currentblock');
+        $('.block').eq(index).addClass('currentblock');
+        
+        $newCurrent.css({left:"-10000px"});
+        $newCurrent.addClass('current');
+
+
+        $newCurrent.animate({left:"0px"},1000,function(){});
+        $current.animate({left:"10000px"},1000,function(){
+            $current.removeClass('current');
+        });
+    });
 }
 
 $(document).ready(main);
