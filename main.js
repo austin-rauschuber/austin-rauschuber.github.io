4fe7308458031dafe70c0718e6da4dea23b29e72
austin-rauschuber <arauschu@purdue.edu>
2018-02-06 22:22:19 -0500

    Fix longstanding bugs and add Emily content

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+var headerColors = ['#50A6C2','#5950c2', '#b250c2', '#c25059', '#c2b450'];
+var publicationsHeaderHeight;
+
+
 var main = function(){
     $(".ui-loader").hide()
-    $('.block').click(function(event){
+    $('.block').click(changeBlock);
+    $('.projectLink').click(scrollToItem);
+
+    calculatePublicationsHeight();
+
+    $(window).resize(calculatePublicationsHeight);
+}
+
+var changeBlock = function () {
         var index = $('.block').index(this);
         var $current = $('.current');
         var textIndex = $('.textbox').index($current);
@@ -9,6 +21,8 @@ var main = function(){
         
         $('.currentblock').removeClass('currentblock');
         $('.block').eq(index).addClass('currentblock');
+
+        $('.header').css("border-top-color", headerColors[index]);
         
         if(index>textIndex){
             $newCurrent.css({left:"10000px"});
@@ -31,20 +45,20 @@ var main = function(){
                 $current.removeClass('current');
             });
         }
-        
-        
-        
-    });
-    
-    
-    
-    $('.projectLink').on("click", function (event){
-         var index = $('.projectLink').index(this);
-        $targetHeader = $('.project').eq(index);
-         $(".projects").animate({ scrollTop: $targetHeader.offset().top + $(".projects").scrollTop() - 120 }, "slow");
-        
-        
-    });
-}
+};
+
+var scrollToItem = function () {
+    var index = $('.projectLink').index(this);
+    $targetHeader = $('.project').eq(index);
+    $(".projects").animate({ scrollTop: $targetHeader[0].offsetTop - publicationsHeaderHeight }, "slow"); 
+};
+
+var calculatePublicationsHeight = function () {
+    $('.textbox:nth-child(3)').css("display", "inline-block");
+    publicationsHeaderHeight = $('.projectHead').height();
+    $('.textbox:nth-child(3)').css("display", "");
+
+    $('.projects').css({'height': 'calc(100% - ' + publicationsHeaderHeight + 'px)'});
+};
 
 $(document).ready(main);
